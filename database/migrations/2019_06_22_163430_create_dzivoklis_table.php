29ff8eab64c7d3465b8621e848f9f8424f3c4169
Ansis Spruģevics <Ansis100@gmail.com>
2019-06-22 19:40:19 +0300

    Fixed migration class naming

--- database/migrations/2019_06_22_163430_create_dzivoklis_table.php
+++ database/migrations/2019_06_22_163430_create_dzivoklis_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateIretsTable extends Migration
+class CreateDzivoklisTable extends Migration
 {
     /**
      * Run the migrations.
