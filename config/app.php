9d76c298b8b0bc1f3a3dccb3048dc4808edb37cf
Ansis Spruģevics <Ansis100@gmail.com>
2019-06-23 09:53:51 +0300

    Reverted app name

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'FindApartment'),
+    'name' => env('APP_NAME', 'Laravel'),
 
     /*
     |--------------------------------------------------------------------------
