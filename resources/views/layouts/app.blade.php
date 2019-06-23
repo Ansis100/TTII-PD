656d4ce973d6fc64e5a9bcf35e3ed3c5b0e64bfa
Ansis Spruģevics <Ansis100@gmail.com>
2019-06-23 09:51:45 +0300

    Created authentication pages; Adjusted views to be divided into sections

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,62 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>{{ config('app.name') }}</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet" type="text/css">
+    <link href="{{ asset('css/style.css') }}" rel="stylesheet" type="text/css">
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+</head>
+
+<body>
+    <div id="app">
+        <!--NavBar v Registration & LogIn-->
+        <div id="PageTop" class="header">
+            <a href="/" class="logo">{{ config('app.name') }}</a>
+            <div class="header-right">
+                <a class="active" href="/">Home</a>
+                <!-- Authentication Links -->
+                @guest
+                <a href="{{ route('login') }}">Login</a>
+                <a href="{{ route('register') }}">Register</a>
+                @else
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true" v-pre>
+                        {{ Auth::user()->name }} <span class="caret"></span>
+                    </a>
+
+                    <ul class="dropdown-menu">
+                        <li>
+                            <a href="{{ route('logout') }}" onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                Logout
+                            </a>
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                {{ csrf_field() }}
+                            </form>
+                        </li>
+                    </ul>
+                </li>
+                @endguest
+            </div>
+        </div>
+        <div class="RightButton">
+            <button class="ToTop_BTN"><a href="#PageTop">To Top</a></button>
+        </div>
+    </div>
+
+    @yield('content')
+    </div>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}"></script>
+</body>
+
+</html>
